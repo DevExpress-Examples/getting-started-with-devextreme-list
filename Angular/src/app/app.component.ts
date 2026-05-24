6c5ff5b84733bca43d0065e462bf237b126c086d
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 11:25:19 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,9 +6,10 @@ import { Product } from './app.types';
 import { AppService } from './app.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   dataSource: DataSource;
